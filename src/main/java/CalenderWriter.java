4f3002554f04d603f803544c3537e9a9043113b1
RobertZetterlund <robert.zetterlund@outlook.com>
2019-03-25 19:52:31 +0100

    added gitignore and reduced code and added comments

--- src/main/java/CalenderWriter.java
+++ src/main/java/CalenderWriter.java
@@ -17,40 +17,12 @@ public class CalenderWriter {
 
 
         System.setProperty("net.fortuna.ical4j.timezone.cache.impl", MapTimeZoneCache.class.getName());
-
         // needs to be here. https://github.com/ical4j/ical4j/issues/195
 
         Calendar calendar = new Calendar();
         calendar.getProperties().add(new ProdId("-//Robert//-"));
         calendar.getProperties().add(Version.VERSION_2_0);
         calendar.getProperties().add(CalScale.GREGORIAN);
-
-
-        if(false) {
-            System.out.println(calendar.toString());
-
-            java.util.Calendar Jcalendar = java.util.Calendar.getInstance();
-            Jcalendar.set(java.util.Calendar.MONTH, java.util.Calendar.DECEMBER);
-            Jcalendar.set(java.util.Calendar.DAY_OF_MONTH, 25);
-
-// initialise as an all-day event..
-            VEvent christmas = new VEvent(new Date(Jcalendar.getTime()), "Christmas Day");
-
-// Generate a UID for the event..
-            FixedUidGenerator ug = new FixedUidGenerator("1");
-            christmas.getProperties().add(ug.generateUid());
-
-            calendar.getComponents().add(christmas);
-
-            System.out.println(calendar.getComponents());
-
-
-            FileOutputStream fout = new FileOutputStream("mycalendar.ics");
-
-            CalendarOutputter outputter = new CalendarOutputter();
-            outputter.output(calendar, fout);
-        }
-
 
         TimeZoneRegistry registry = TimeZoneRegistryFactory.getInstance().createRegistry();
         TimeZone timezone = registry.getTimeZone("Europe/Oslo");
@@ -77,7 +49,7 @@ public class CalenderWriter {
         endDate.set(java.util.Calendar.MINUTE, 45);
         endDate.set(java.util.Calendar.SECOND, 0);
 
-// Create the event
+        // Create the event
         String eventName = "Ica Jobb";
         DateTime start = new DateTime(startDate.getTime());
         DateTime end = new DateTime(endDate.getTime());
@@ -86,32 +58,28 @@ public class CalenderWriter {
         // add timezone info..
         meeting.getProperties().add(tz.getTimeZoneId());
 
-// generate unique identifier..
+        // generate unique identifier..
 
         FixedUidGenerator ug = new FixedUidGenerator("6");
-        //Uid uid = ug.generateUid();
         meeting.getProperties().add(ug.generateUid());
 
-
-
-// Create a calendar
+        // Create a calendar
         net.fortuna.ical4j.model.Calendar icsCalendar = new net.fortuna.ical4j.model.Calendar();
         icsCalendar.getProperties().add(new ProdId("-//Ica Schema Rz//"));
         icsCalendar.getProperties().add(CalScale.GREGORIAN);
 
+        // add the meeting
         icsCalendar.getComponents().add(meeting);
-        System.out.println(icsCalendar);
 
-
-        FileOutputStream fout = new FileOutputStream("meeting.ics");
-
+        // set version (mandatory)
         icsCalendar.getProperties().add(Version.VERSION_2_0);
+
+        // generate a file that can be emailed
+        FileOutputStream fout = new FileOutputStream("meeting.ics");
         CalendarOutputter outputter = new CalendarOutputter();
         outputter.output(icsCalendar, fout);
 
-
         fout.close();
-
 
     }
 }
